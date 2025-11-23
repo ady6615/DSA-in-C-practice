5578a8d1fe0b7f0e0eb349d5400956ddf6135d27
ady6615 <advaitpatil379@gmail.com>
2025-11-23 22:23:45 +0530

    structure and function in c changing to  class

--- Basics/structureandfunctioninc.cpp
+++ Basics/structureandfunctioninc.cpp
@@ -4,31 +4,68 @@
 
 using namespace std;
 
-struct Rectangle{
-    int lenght;
+// struct Rectangle{
+//     int lenght;
+//     int breadth;
+// };
+
+// void initialize(struct Rectangle *r,int l,int b){
+//     r->lenght=l;
+//     r->breadth=b;
+// }
+
+// int area(struct Rectangle *r){
+//         return (r->lenght)*(r->breadth);
+// }
+
+// void changeLength(struct Rectangle *r,int l){
+//     r->lenght=l;
+// }
+// void changeBreadth(struct Rectangle *r,int b){
+//     r->breadth=b;
+// }
+
+// int main(){
+//     struct Rectangle r;
+//     initialize(&r,10,5);
+//     changeBreadth(&r,10);
+//     changeLength(&r,20);
+//     cout<<area(&r)<<endl;
+// }
+
+class Rectangle
+{
+    private:
+    int length;
     int breadth;
+
+    public:
+    Rectangle(int l,int b)
+    {
+        length=l;
+        breadth=b;
+    }
+
+    int area()
+    {
+        return length*breadth;
+    }
+
+    void ChangeLength(int l)
+    {
+        length=l;
+    }
+
+    void ChangeBreadth(int b)
+    {
+        breadth=b;
+    }
+
 };
-
-void initialize(struct Rectangle *r,int l,int b){
-    r->lenght=l;
-    r->breadth=b;
-}
-
-int area(struct Rectangle *r){
-        return (r->lenght)*(r->breadth);
-}
-
-void changeLength(struct Rectangle *r,int l){
-    r->lenght=l;
-}
-void changeBreadth(struct Rectangle *r,int b){
-    r->breadth=b;
-}
-
 int main(){
-    struct Rectangle r;
-    initialize(&r,10,5);
-    changeBreadth(&r,10);
-    changeLength(&r,20);
-    cout<<area(&r)<<endl;
+    Rectangle r(10,5);
+    cout<<r.area()<<endl;
+    r.ChangeLength(34);
+    r.ChangeBreadth(24);
+    cout<<r.area()<<endl;
 }
